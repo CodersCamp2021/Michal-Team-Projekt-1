6c456802a9fec9c4881a60e235a7e0889af7e969
Justyna Bodurka <bodurkajustyna@gmail.com>
2021-12-17 12:41:04 +0100

    feat:adjustment after review

--- src/app/actionButtons.js
+++ src/app/actionButtons.js
@@ -5,7 +5,7 @@ import starshipsTemplate from './templates/starshipsRules.template.js';
 
 export default () => {
   const vehiclesButton = document.querySelector('.mode-btn--vehicles');
-  const contentContainer = document.querySelector('.content__container');
+  const contentContainer = document.querySelector('#content__container');
   const peopleButton = document.querySelector('.mode-btn--people');
   const starshipsButton = document.querySelector('.mode-btn--starships');
   const rankingButton = document.querySelector('.button--ranking');
