22d6a1e4a7f6b56ab75fd73be374b50442abddfc
Weroniika <40180443+Weroniika@users.noreply.github.com>
2021-12-20 14:04:43 +0100

    feat: Add question generator (#24)
    * feat: Add question generator
    
    * Fix answers ids, change base_url to swapi.dev
    
    * Improve env variables after review
    
    * Comment out test with error

---
+++ src/app/questionGenerator.js
@@ -0,0 +1,33 @@
+import { PEOPLE_IDS, STARSHIPS_IDS, VEHICLES_IDS } from './constants/modeIds';
+import { getOneById } from './api/starwarsApi';
+import { generateRandomId, generateRandomIds } from './shared/random';
+
+const getModeIds = (mode) => {
+  switch (mode) {
+    case 'people':
+      return PEOPLE_IDS;
+    case 'vehicles':
+      return VEHICLES_IDS;
+    case 'starships':
+      return STARSHIPS_IDS;
+  }
+};
+
+export const generateQuestion = async (mode) => {
+  const modeIdsArray = getModeIds(mode);
+  const answersIds = generateRandomIds(modeIdsArray);
+  const rightAnswerId = generateRandomId(answersIds);
+  const answers = await Promise.all(
+    answersIds.map(async (id) => {
+      const answersData = await getOneById(mode, id);
+      return {
+        id,
+        ...answersData,
+      };
+    }),
+  );
+  const rightAnswer = answers.find((answer) => answer.id === rightAnswerId);
+  const answerImgPath = `static/assets/img/modes/${mode}/${rightAnswerId}.jpg`;
+
+  return { answers, answerImgPath, rightAnswer };
+};
