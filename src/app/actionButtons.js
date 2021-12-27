c45c6212a692874a2b0fd2167f6fecf8e844e924
Justyna Bodurka <bodurkajustyna@gmail.com>
2021-12-27 12:42:52 +0100

    refactor: transfer const modes to constants.js

--- src/app/actionButtons.js
+++ src/app/actionButtons.js
@@ -3,6 +3,7 @@ import { quizView } from './templates/quiz.template.js';
 import schoolSvg from '../../static/assets/ui/icon-shool.svg';
 import { generateQuestion } from './questionGenerator.js';
 import { rulesView } from './templates/rules.template.js';
+import { modes } from './constants';
 
 export const actionButtons = () => {
   const vehiclesButton = document.querySelector('.mode-btn--vehicles');
@@ -13,21 +14,6 @@ export const actionButtons = () => {
   const rankingButton = document.querySelector('.button--ranking');
   const actionButton = document.querySelector('.button--action');
   const quizPhoto = document.querySelector('.photo__container img');
-  const modes = {
-    title: {
-      vehicles: 'Do you recognize this vehicle?',
-      people: 'Who is this character?',
-      starships: 'Do you recognize this starship?',
-    },
-    content: {
-      vehicles:
-        'You have two minute (2m) to answer as many questions as possible. During the game on each question you need to select which vehicle from Star Wars is showed on the left.',
-      starships:
-        'You have two minutes (2m) to answer as many questions as possible. During the game on each question you need to select which starship from Star Wars is showed on the left.',
-      people:
-        'You have two minutes (2m) to answer as many questions as possible. During the game on each question you need to select who from Star Wars is showed on the left (Jar Jar Binks right now) from available options.',
-    },
-  };
 
   actionButton.setAttribute('title', modes.title.people);
   actionButton.setAttribute('mode', 'people');
