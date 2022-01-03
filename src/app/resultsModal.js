430445291e77c8f2f3ff3e83e129fcdf320fa2c0
Weroniika <40180443+Weroniika@users.noreply.github.com>
2022-01-04 00:03:56 +0100

    refactor: change game modal data (#48)
    * refactor: change game modal data
    
    * fix: delete class
    
    Co-authored-by: Mateusz Binięda <93389452+Arssin@users.noreply.github.com>

--- src/app/resultsModal.js
+++ src/app/resultsModal.js
@@ -1,10 +1,11 @@
 import { resultsModalView } from './templates/resultsModal.template';
 
-export const renderResultsModal = ({ playerData = [], correctAnswers = [], computerData = [] }) => {
-  const container = document.querySelector('#content__container');
+export const renderResultsModal = ({ humanPlayer, computerPlayer }) => {
+  const container = document.querySelector('main');
+  const playerData = { playerScore: humanPlayer.score, playerAnswers: humanPlayer.answers };
+  const computerData = { computerScore: computerPlayer.score, computerAnswers: computerPlayer.answers };
   container.innerHTML = resultsModalView({
     playerData,
-    correctAnswers,
     computerData,
   });
   const closeBtns = document.querySelectorAll('.close-modal-btns');
