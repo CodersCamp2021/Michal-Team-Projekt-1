a5293ccae4d045763493f22b4fe714d9d2ca51be
Karol Wolski <karolwolski04@gmail.com>
2022-01-04 01:36:25 +0100

    fix(resultsModal): remove 'correctAnswers' from argument to function resultModalView

--- src/app/resultsModal.js
+++ src/app/resultsModal.js
@@ -11,7 +11,6 @@ export const renderResultsModal = ({ humanPlayer, computerPlayer }) => {
     'afterend',
     resultsModalView({
       playerData,
-      correctAnswers,
       computerData,
     }),
   );
