3a1d5334a800b9ea5b3ca1e4170ff164fdd8b047
Kamil Kantorski <3733349+KamiKant@users.noreply.github.com>
2022-01-05 00:55:03 +0100

    minor fixes

--- src/app/counterTimer.js
+++ src/app/counterTimer.js
@@ -1,20 +1,18 @@
 import { renderResultsModal } from './resultsModal';
-import { humanPlayer } from './players';
-import { computerPlayer } from './players';
+import { humanPlayer, computerPlayer } from './players';
 
 const showTimer = (time) => {
   const timer = document.querySelector('.timer');
   const blade = document.querySelector('.sword-blade');
   timer.textContent = `Time left: ${Math.floor(time / 1000 / 60)}m ${Math.floor((time / 1000) % 60)}s`;
-  blade.value = 120 - Math.floor(time / 1000);
+  blade.value = Math.floor(time / 1000);
 };
 
 export const counterTimer = () => {
-  // Current time + 2 minutes
   const time = Date.now() + 120 * 1000;
   const countTime = setInterval(() => {
     const timeLeft = time - Date.now();
-    if (timeLeft < 0) {
+    if (timeLeft < 1000) {
       clearInterval(countTime);
       renderResultsModal({ humanPlayer, computerPlayer });
     }
